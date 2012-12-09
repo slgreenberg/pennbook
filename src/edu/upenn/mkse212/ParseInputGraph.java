da76ea3fc414d180d37b3b4550968cbc87c6fc9a
Susan <susangr@seas.upenn.edu>
2012-12-09 18:32:14 -0500

    copied hw4 files

---
+++ src/edu/upenn/mkse212/ParseInputGraph.java
@@ -0,0 +1,45 @@
+package edu.upenn.mkse212;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import edu.upenn.mkse212.IKeyValueStorage;
+import edu.upenn.mkse212.KeyValueStoreFactory;
+
+public class ParseInputGraph {
+	
+  public static void main(String[] args) throws IOException {
+    IKeyValueStorage socialGraph = KeyValueStoreFactory.getKeyValueStore(KeyValueStoreFactory.STORETYPE.BERKELEY, 
+		    "socialGraph", "/home/mkse212/bdb/", "user", "authKey", false);
+
+    File here = new File(".");
+
+    int count = 0;
+
+    //while there are still lines to be read, the buffered reader
+    //takes in a line and splits on a tab. it then put the node (arr[0])
+    //and the node it points to (arr[1]) and puts them into the
+    //key value storage and increments the count
+    for ( String fname : here.list()) {
+      if (fname.startsWith("adsorption")) {
+    	  System.out.println("Parsing " + fname + "...");
+    	  BufferedReader br = new BufferedReader(new FileReader(fname));
+    	  while (br.ready()) {
+    		  String line = br.readLine();
+    		  String[] arr = line.split("\t");
+    		  String node = arr[0];
+    		  String points = arr[1];
+			socialGraph.put(node, points);
+			count++;
+    	  }
+
+      }
+    }
+
+    System.out.println("\nLoaded " + count + " edges");
+
+    socialGraph.close();
+  }
+}
